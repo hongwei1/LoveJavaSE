551b4df2ebf348a695075d0fd09299a5861564e9
zhw110 <zhw_gz@126.com>
2016-08-30 21:15:25 +0200

    : day20-day22 IO
    Task-Url: https://docs.google.com/spreadsheets/d/1uRbktO2HuytsC59-1gGWWNTMlPHyTQ04O439SZ0iNyA/edit#gid=737558950

--- src/main/java/itcastday02/VarDemo.java
+++ src/main/java/itcastday02/VarDemo.java
@@ -1,11 +1,17 @@
 package itcastday02;
+
+import java.io.InputStream;
+
 /**
- * --day02 02-Java基础语法1 P1-P15
- * Test to define all the variables: values ,char,fraction and reference
- * 
- * */
-class VarDemo {
-	public static void main(String[] args) {
+ * --day02 02-Java基础语法1 P1-P15 Test to define all the variables: values ,char,fraction and reference
+ *
+ */
+class VarDemo
+{
+	public static void main(String[] args)
+	{
+
+		InputStream in = System.in;
 		// practice all the varibales
 		// Value
 		byte bByte = 1;
@@ -15,13 +21,15 @@ class VarDemo {
 		// char
 		char cCharVa = '1';
 		char cCharVa1 = 'b';
+		char cCharVa2 = 400;
+		System.out.println(cCharVa2);
 		// fractions
 		float fFloatValure = 5;
 		double dDouble = 6;
 		// reference
-			// class
-			// interface
-			// Array
+		// class
+		// interface
+		// Array
 
 		// 数据类型 变量名 = 初始化值;
 		byte b2 = 3;
--- src/main/java/itcastday20/p2/io/filewriter/IOExceptionDemo.java
+++ src/main/java/itcastday20/p2/io/filewriter/IOExceptionDemo.java
@@ -3,51 +3,51 @@ package itcastday20.p2.io.filewriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class IOExceptionDemo {
+public class IOExceptionDemo
+{
 
 	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args)
+	{
+		// elder one :
 		FileWriter fw = null;
-		try {
+		try
+		{
+			fw = new FileWriter("/demo1.txt");
+			fw.write("abcde" + IOExceptionDemo.LINE_SEPARATOR + "hahaha");
 
-			fw = new FileWriter("/bdemo.txt");
-
-			fw.write("abcde" + LINE_SEPARATOR + "hahaha");
-
-		} catch (IOException e) {
-			System.out.println(e.toString());
-		} finally {
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
 			if (fw != null)
-				try {
+			{
+				try
+				{
 					fw.close();
-				} catch (IOException e) {
-					// code....
+				}
+				catch (IOException e)
+				{
 					throw new RuntimeException("πÿ±’ ß∞‹");
 				}
+			}
 		}
 
-		// my heaven
-//		 myHeaven();
-	}
+		// JDK1.5
+		try (FileWriter fw2 = new FileWriter("/demo2.txt"))
+		{
+			fw2.write("abcde" + IOExceptionDemo.LINE_SEPARATOR + "hahaha");
 
-	private static void myHeaven() {
-		FileWriter fw = null;
-		try {
-			fw = new FileWriter("/demo.txt");
-			fw.write("abcde" + LINE_SEPARATOR + "hahaha");
-			
-		} catch (Exception e) {
-
-		}finally {
-			if(fw!=null)
-				try {
-					fw.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
 		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
 	}
 
 }
