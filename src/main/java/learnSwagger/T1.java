6d67b63d1dd19e480d4e6a554fab02dd4d133c28
hongwei1 <hhh@tesobe.com>
2017-10-30 21:23:39 +0100

    Learn Swagger

---
+++ src/main/java/learnSwagger/T1.java
@@ -0,0 +1,30 @@
+package learnSwagger;
+import io.swagger.parser.SwaggerParser;
+import io.swagger.models.Swagger;
+import io.swagger.parser.util.SwaggerDeserializationResult;
+import io.swagger.util.Json;
+
+import java.util.List;
+
+// ... your code
+
+// read a swagger description from the petstore
+  
+public class T1
+{
+	public static void main(String[] strings)
+	{
+		Swagger swagger = new SwaggerParser().read("/Users/zhanghongwei/Documents/workspaceScala/LoveJavaSE/src/main/resources/1.json");
+//		Swagger swagger = new SwaggerParser().read("http://petstore.swagger.io/v2/swagger.json");
+
+		SwaggerDeserializationResult result=new SwaggerParser().readWithInfo(Json.pretty(swagger));
+		List<String> message=result.getMessages();
+			System.out.println("result : "+result);
+		for(String m:message)
+		{
+			System.out.println("result : "+m);
+			System.out.println("result : "+m.length());
+		}
+	}
+
+}
