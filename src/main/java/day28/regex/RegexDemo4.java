1268da02e5060354d459adbd856ad697708091cf
hongwei1 <hhh@tesobe.com>
2021-05-05 23:54:36 +0200

    finished a blog

--- src/main/java/day28/regex/RegexDemo4.java
+++ src/main/java/day28/regex/RegexDemo4.java
@@ -6,7 +6,15 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 //https://www.bilibili.com/s/video/BV1py4y1Y7iQ
-//Java反射：泛型擦除
+// 泛型本质是一门编译时期的技术,专门给"编译器"使用的.
+//Java反射：
+// 泛型擦除:
+// classloader 记载类时,JVM会把该类中的泛型擦除(擦除之后就是 Object类型)
+// 泛型补偿:
+//   获取莫个元素时, JVM会根据元素的"实际类型" 来进行签字类型转换.
+// a)擦除和补偿 都是有 JVM 来完成的,不用人手动去管!
+// b)擦除和补偿 都是在运行时期完成的,不是在编译时期!
+//
 
 public class RegexDemo4 {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
